0f622f29fbfad96d032e1eba605f47d4cb4e8b8e
ZhiLong25 <tanzhilong25@gmail.com>
2024-01-10 19:29:23 +0800

    E

--- server/Controllers/QueryController.cs
+++ server/Controllers/QueryController.cs
@@ -1,39 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using NTUCClub.Models;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using NTUCClub.Models;
 
-namespace NTUCClub.Controllers
-{
-    [ApiController]
-    [Route("[controller]")]
-    public class QueryController : ControllerBase
-    {
-        private readonly MyDbContext _context;
+//namespace NTUCClub.Controllers
+//{
+//    [ApiController]
+//    [Route("[controller]")]
+//    public class QueryController : ControllerBase
+//    {
+//        private readonly MyDbContext _context;
 
-        public QueryController(MyDbContext context)
-        {
-            _context = context;
-        }
+//        public QueryController(MyDbContext context)
+//        {
+//            _context = context;
+//        }
 
-        [HttpPost("Addquery")]
+//        [HttpPost("Addquery")]
 
-        public IActionResult AddQuery(Query query)
-        {
-            Console.WriteLine("type here");
-            var now = DateTime.Now;
+//        public IActionResult AddQuery(Query query)
+//        {
+//            Console.WriteLine("type here");
+//            var now = DateTime.Now;
 
-            var myQuery = new Query()
-            {
-                Email = query.Email.Trim(),
-                QuerySubject = query.QuerySubject.Trim(),
-                QueryDescription = query.QueryDescription.Trim(),
-                CreatedAt = now,
-                UpdatedAt = now
-            };
-            _context.Vouchers.Add(myQuery);
-            _context.SaveChanges();
-            return Ok(myQuery);
-        }
+//            var myQuery = new Query()
+//            {
+//                Email = query.Email.Trim(),
+//                QuerySubject = query.QuerySubject.Trim(),
+//                QueryDescription = query.QueryDescription.Trim(),
+//                CreatedAt = now,
+//                UpdatedAt = now
+//            };
+//            _context.Vouchers.Add(myQuery);
+//            _context.SaveChanges();
+//            return Ok(myQuery);
+//        }
 
 
-    }
-}
+//    }
+//}
